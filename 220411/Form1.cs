b5cb8c02eeb1d519ed6f7b937a9cc081ad298542
user <user@DESKTOP-6JBEF5K>
2022-04-11 11:09:11 +0800

    塗鴉牆

---
+++ 220411/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.VisualBasic.PowerPacks;
+
+namespace _220411
+{
+    public partial class Form1 : Form
+    {
+        UdpClient U;
+        Thread Th;
+
+        ShapeContainer C;//畫布物件
+        ShapeContainer D;//畫布物件
+        Point stP;//繪圖起點
+        string p;//筆畫座標字串
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            C = new ShapeContainer();
+            this.Controls.Add(C);//建立畫布
+            D = new ShapeContainer();//加入畫步C到Form1
+            this.Controls.Add(D);//加入畫布D到Form1
+        }
+
+        private void Listen() 
+        {
+            int Port = int.Parse(textBox_listenPort.Text);
+            U = new UdpClient(Port);
+
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), Port);
+
+            while(true)
+            {
+                byte[] B = U.Receive(ref EP);
+                string A = Encoding.Default.GetString(B);
+                string[] Q = A.Split('/');
+                Point[] R = new Point[Q.Length];
+                for(int i = 0; i < Q.Length; i++)
+                {
+                    string[] K = Q[i].Split(',');
+                    R[i].X = int.Parse(K[0]);
+                    R[i].Y = int.Parse(K[1]);
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender,FormClosingEventArgs e) 
+        {
+            try
+            {
+                Th.Abort();
+                U.Close();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            Th = new Thread(Listen);
+            Th.Start();
+            button1.Enabled = false;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            stP = e.Location;
+            p = stP.X.ToString() + "," + stP.Y.ToString();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                LineShape L = new LineShape();//建立線段物件
+                L.StartPoint = stP;//線段起點
+                L.EndPoint = e.Location;//線段終點
+                L.Parent = C;//線段加入畫布C
+                stP = e.Location;//終點變起點
+                p += "/" + stP.X.ToString() + "," + stP.Y.ToString();//持續記錄座標
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            int Port = int.Parse(textBox_Port.Text);
+            UdpClient S = new UdpClient(textBox_ip.Text,Port);
+            byte[] B = Encoding.Default.GetBytes(p);
+            S.Send(B, B.Length);
+            S.Close();
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_listenPort_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
